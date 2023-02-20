69b0dc1f0a5a7f4ea9ebb462ed2397e7c1bedfdf
Onee_Only <kimww0306@gmail.com>
2023-02-20 13:47:07 +0900

    indexing 사용

--- db/index.go
+++ db/index.go
@@ -8,6 +8,10 @@ import (
 	bolt "go.etcd.io/bbolt"
 )
 
+const (
+	iterateSize = 30
+)
+
 type IndexTx struct {
 	From string
 	To   string
@@ -56,4 +60,130 @@ func AddIndex(height int, hash string, txs []IndexTx) {
 		b.Put([]byte(strconv.Itoa(height)), lib.ToBytes(idx))
 		return nil
 	})
+}
+
+func FindHashByHeight(height int) string {
+	hash := ""
+	DB.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte(properties.BucketIndex)).Cursor()
+		_, v := c.Seek([]byte(strconv.Itoa(height)))
+		if v != nil {
+			idx := &index{}
+			lib.FromBytes(idx, v)
+			hash = idx.hash
+		}
+		return nil
+	})
+	return hash
+}
+
+func FindHashesFrom(publicKey string) []string {
+	hashes := []string{}
+	DB.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(properties.BucketIndex))
+
+		amount := FindCurrentHeight()
+		ch := make(chan string)
+		done := make(chan bool)
+		cnt := 0
+
+		for start := 1; start >= amount; start += iterateSize {
+			cnt++
+			go iterate(b.Cursor(), ch, done, start, publicKey, "")
+		}
+		for {
+			select {
+			case hash := <-ch:
+				hashes = append(hashes, hash)
+			case <-done:
+				cnt--
+				if cnt == 0 {
+					return nil
+				}
+			}
+		}
+	})
+	return hashes
+}
+
+func FindHashesTo(publicKey string) []string {
+	hashes := []string{}
+	DB.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(properties.BucketIndex))
+
+		amount := FindCurrentHeight()
+		ch := make(chan string)
+		done := make(chan bool)
+		cnt := 0
+
+		for start := 1; start >= amount; start += iterateSize {
+			cnt++
+			go iterate(b.Cursor(), ch, done, start, "", publicKey)
+		}
+		for {
+			select {
+			case hash := <-ch:
+				hashes = append(hashes, hash)
+			case <-done:
+				cnt--
+				if cnt == 0 {
+					return nil
+				}
+			}
+		}
+	})
+	return hashes
+}
+
+func FindHashesAll(publicKey string) []string {
+	hashes := []string{}
+	DB.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(properties.BucketIndex))
+
+		amount := FindCurrentHeight()
+		ch := make(chan string)
+		done := make(chan bool)
+		cnt := 0
+
+		for start := 1; start >= amount; start += iterateSize {
+			cnt++
+			go iterate(b.Cursor(), ch, done, start, publicKey, publicKey)
+		}
+		for {
+			select {
+			case hash := <-ch:
+				hashes = append(hashes, hash)
+			case <-done:
+				cnt--
+				if cnt == 0 {
+					return nil
+				}
+			}
+		}
+	})
+	return hashes
+}
+
+func iterate(cursor *bolt.Cursor, ch chan<- string, done chan<- bool, start int, from, to string) {
+	cnt := 0
+	idx := &index{}
+	_, v := cursor.Seek([]byte(strconv.Itoa(start)))
+	for {
+		if cnt == iterateSize || v == nil {
+			done <- true
+			return
+		}
+		lib.FromBytes(idx, v)
+
+		for _, tx := range idx.txs {
+			if from != "" && tx.From == from {
+				ch <- idx.hash
+			} else if to != "" && tx.To == to {
+				ch <- idx.hash
+			}
+		}
+
+		_, v = cursor.Next()
+		cnt++
+	}
 }
